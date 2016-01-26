e9b9a6e2ce667f9d22ac5a979e58b0849d2a2f8b
Ranjan Shrestha <ranjan@sharingapples.com>
2016-01-26 18:54:38 +0545

    Started working on NepaliCalendar

---
+++ src/main/java/com/sharingapples/calendar/nepal/NepaliCalendar.java
@@ -0,0 +1,10 @@
+package com.sharingapples.calendar.nepal;
+
+/**
+ * A NepaliCalendar library for converting between Gregorian calendar and
+ * Nepali Calendar (Bikram Samvat).
+ *
+ * Created by ranjan on 1/26/16.
+ */
+public class NepaliCalendar {
+}
